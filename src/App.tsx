18a8725337ea0dc75c7d9f686eb3c0fa0665c63b
JuWi2517 <jenik.richter@gmail.com>
2025-10-27 02:21:12 +0100

    fixed router

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,24 @@
-// You need this import for 'React.ReactElement'
-import React from 'react';
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
-import PublicPage from './pages/PublicPage';
-import LoginPage from './pages/LoginPage';
-import AdminPage from './pages/AdminPage';
-import { auth } from './firebase';
-import './css/App.css';
-
+import React from "react";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import PublicPage from "./pages/PublicPage";
+import LoginPage from "./pages/LoginPage";
+import AdminPage from "./pages/AdminPage";
+import { auth } from "./firebase";
+import "./css/App.css";
 
 function PrivateRoute({ children }: { children: React.ReactElement }) {
-    return auth.currentUser ? children : <Navigate to="/admin/login" />;
+    return auth.currentUser ? children : <Navigate to="/admin/login" replace />;
 }
 
 export default function App() {
     return (
-        <>
-
-            <HashRouter>
-                <Routes>
-                    <Route index element={<PublicPage />} />
-                    <Route path="admin/login" element={<LoginPage />} />
-                    <Route
-                        path="admin/*"
-                        element={<PrivateRoute><AdminPage /></PrivateRoute>}
-                    />
-                    <Route path="*" element={<Navigate to="/" replace />} />
-                </Routes>
-            </HashRouter>
-        </>
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<PublicPage />} />
+                <Route path="/admin/login" element={<LoginPage />} />
+                <Route path="/admin/*" element={<PrivateRoute><AdminPage /></PrivateRoute>} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </HashRouter>
     );
 }

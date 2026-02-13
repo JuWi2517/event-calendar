785220e778b18d05c89bd51b3b1bc5f94fd7e110
JuWi2517 <jenik.richter@gmail.com>
2026-02-13 23:56:31 +0100

    speed optimalization

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
             ],
             exclude: ['/moje-akce', '/admin/dashboard'],
             readable: true,
+            generateRobotsTxt: false,
         }),
     ],
 })
